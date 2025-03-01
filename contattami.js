f1ca67a0c887db2348b97f0cf6511a7187eda048
france5cogreen <francesco.di.rienzo.00@gmail.com>
2025-03-01 01:27:30 +0100

    Added some graphic and functionality fixes

--- contattami.js
+++ contattami.js
@@ -4,14 +4,9 @@ const contactFormContainer = document.getElementById('contact-form-container');
 
 video.addEventListener('ended', function() {
   contactButton.classList.remove('hidden');
-  contactButton.classList.add('show');
+  contactButton.classList.add('show'); // Aggiunge l'animazione
 });
 
 contactButton.addEventListener('click', function() {
   contactFormContainer.classList.remove('hidden');
-});
-
-document.getElementById('contact-form').addEventListener('submit', function(event) {
-  event.preventDefault();
-  document.getElementById('success-message').classList.remove('hidden');
 });
